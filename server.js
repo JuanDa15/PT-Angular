6892ff6c9fbb87cd1923baa8de8aaf3387c8e0b2
Juan David Osorio Ortiz <jdoo1115@gmail.com>
2021-12-22 23:13:30 -0500

    .

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const app = express();
 app.use(express.static('./dist/PT'));
 
 app.get('/*', (req, res) =>
-    res.sendFile('index.html', {root: 'dist/PT/index.html'}),
+    res.sendFile('index.html', {root: 'dist/PT/'}),
 );
 
 // Start the app by listening on the default Heroku port
